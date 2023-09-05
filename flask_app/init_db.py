f5942fd551ac1ba5ad8585fe6f77c2f06c84b7c4
MadMax-G <123890543+MadMax-G@users.noreply.github.com>
2023-09-05 18:58:39 +0300

    The flask app

---
+++ flask_app/init_db.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import psycopg2
+
+open_port = os.environ["APP_PORT"]
+db_host = os.environ["DB_IP"]
+db_pass = os.environ["DB_PASS"]
+
+conn = psycopg2.connect(host=db_host,
+                        database='flask_db',
+                        user="maxim",
+                        password=db_pass)
+
+# Open a cursor to perform database operation
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute('DROP TABLE IF EXISTS books;')
+cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+                                 'title varchar (150) NOT NULL,'
+                                 'author varchar (50) NOT NULL,'
+                                 'pages_num integer NOT NULL,'
+                                 'review text,'
+                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
+
+# Insert data into the table
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('A Tale of Two Cities',
+             'Charles Dickens',
+             489,
+             'A great classic!')
+            )
+
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('Anna Karenina',
+             'Leo Tolstoy',
+             864,
+             'Another great classic!')
+            )
+
+conn.commit()
+
+cur.close()
+conn.close()
